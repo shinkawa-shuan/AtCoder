0a1a7e3b826a08d2c9c0c2ddbbc1b491966809a5
suneemon1 <suneemon.com@gmail.com>
2022-03-28 18:32:42 +0900

    change

--- onlinejudge/cptest.sh
+++ onlinejudge/cptest.sh
@@ -6,8 +6,10 @@ base_url=${problem_name%_*}
 code_path=$2
 
 # make test directory
-if [ ! -e ${test_dir} ]; then
-    oj dl -d ${test_dir} https://atcoder.jp/contests/${base_url}/tasks/${problem_name//-/_}
+if [ ! -e "${test_dir}" ]; then
+    oj dl -d "${test_dir}" https://atcoder.jp/contests/"${base_url}"/tasks/"${problem_name//-/_}"
 fi
 
-g++ -std=c++14 -Wall -Wextra -O2 ${code_path} && oj test -c "./a.out " -d ${test_dir}
+g++ -std=c++14 "${code_path}" && oj test -c "./a.out " -d "${test_dir}"
+
+#-Wall -Wextra -O2
